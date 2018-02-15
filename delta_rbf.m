76b12c638bce46436119d0eae5058e895396a4c0
GIF1 <gif1@hi.is>
2018-02-15 12:11:07 +0100

    Sina's updates

--- delta_rbf.m
+++ delta_rbf.m
@@ -4,31 +4,37 @@ function [train_error] = delta_rbf(train_vect, train_sin, test_vect, test_sin, s
 %mui is the middle of the RBF, we place it at the sinusfunction.
 mui = get_mui(train_vect,train_sin,units);
 
-%create a phi matrix
-rbf = GaussianRBF(train_sin, mui, sigma);
-rbf = rbf';
-
 %Initialise weights
 weights = zeros(1,units);
 
 for iter = 1:epochs
+    %Shuffle the data by random for each epoch
+    shuffle = randperm(length(train_vect));
+    train_sin = train_sin(:,shuffle);
+    test_sin = test_sin(:,shuffle);
     
-    %calculate error for each epoch
-    train_error(iter) = mean(abs(weights*rbf - train_sin));
+    %create a phi matrix
+    rbf = GaussianRBF(train_sin, mui, sigma);
+    rbf = rbf';
     
-    %instantanious error xi(estimated error is "target")
-    xi = 0.5 .* (train_sin - weights*rbf).^2;
+    for i = 1:length(test_sin)
     
-    %weight update
-    delta_weights = (eta.*(train_sin - weights*rbf))*rbf';
-    weights = weights + delta_weights;
-    
-    %test the data in each epoch
-    test_mui = get_mui(test_vect,test_sin,units);
-    test_rbf = GaussianRBF([test_vect; test_sin], test_mui ,sigma);
-    test_rbf = test_rbf';
-    test_f = weights*test_rbf;
-     
+        %instantanious error xi(estimated error)
+        e = train_sin(i) - weights*rbf(:,i);
+        %xi(iter) = 0.5.*e.^2;
+        
+        %weight update
+        delta_weights = (eta.*e)*rbf(:,i)';
+        weights = weights + delta_weights;
+        
+        %test the data in each epoch
+        test_mui = get_mui(test_vect,test_sin,units);
+        test_rbf = GaussianRBF([test_vect; test_sin], test_mui ,sigma);
+        test_rbf = test_rbf';
+        test_f = weights*test_rbf;
+    end
+    train_error(iter) = mean(abs(train_sin - weights*rbf));
 end
 
-train_error = mean(train_error);
+%error of the last epoch for comparison
+train_error = train_error(end);
