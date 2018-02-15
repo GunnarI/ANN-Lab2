3b20d61cf590fa6f3a5f510bf3f0f2161cf72939
snorrirafn <snorri14@ru.is>
2018-02-15 12:01:57 +0100

    added a point

--- get_mui.m
+++ get_mui.m
@@ -1,6 +1,6 @@
 function [ mui ] = get_mui(x,y,units)
 
-%mui is the middle of the RBF, we place it at the sinusfunction.
+%mui is the middle of the RBF, we place it at the sinusfunction..
 x_stepsize = round(linspace(1,length(x),units));
 y_stepsize = round(linspace(1,length(y),units));
 mui = [x(x_stepsize); y(y_stepsize)];
