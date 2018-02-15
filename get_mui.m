8e34feb81a9c23fcd47bc41411a5717c0d9fc6a0
sinorious <erndwein@kth.se>
2018-02-15 11:11:57 +0100

    fixed it

--- get_mui.m
+++ get_mui.m
@@ -1,7 +1,9 @@
 function [ mui ] = get_mui(x,y,units)
 
 %mui is the middle of the RBF, we place it at the sinusfunction.
-mui = [x(1:ceil(length(x)/units):end); y(1:ceil(length(y)/units):end)];
+x_stepsize = round(linspace(1,length(x),units));
+y_stepsize = round(linspace(1,length(y),units));
+mui = [x(x_stepsize); y(y_stepsize)];
 
 end
 
